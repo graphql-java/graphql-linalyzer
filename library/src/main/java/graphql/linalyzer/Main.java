bab185adb20287a514d76f7ba1ca8d8658abcd8b
Andreas Marek <andimarek@fastmail.fm>
2018-11-10 10:12:03 +1100

    add Main

---
+++ library/src/main/java/graphql/linalyzer/Main.java
@@ -0,0 +1,14 @@
+package graphql.linalyzer;
+
+import graphql.language.AstPrinter;
+import graphql.language.Document;
+import graphql.parser.Parser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String schema = "type Query{foo:String}";
+        Document document = new Parser().parseDocument(schema);
+        System.out.println(AstPrinter.printAst(document));
+    }
+}
