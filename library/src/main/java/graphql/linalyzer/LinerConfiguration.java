71d192b24fc1d6171cfd6370686f22347f0878ab
Andreas Marek <andimarek@fastmail.fm>
2018-11-10 10:00:38 +1100

    wip

--- library/src/main/java/graphql/linalyzer/LinerConfiguration.java
+++
@@ -1,6 +0,0 @@
-package graphql.linalyzer;
-
-public class LinerConfiguration {
-
-
-}
--- library/src/main/java/graphql/linalyzer/rules/NamingRule.java
+++ library/src/main/java/graphql/linalyzer/rules/NamingRule.java
@@ -4,8 +4,11 @@ import graphql.linalyzer.LinterRule;
 import graphql.linalyzer.LinterRuleResult;
 import graphql.linalyzer.SchemaDefinition;
 
+import java.util.regex.Pattern;
+
 public class NamingRule implements LinterRule {
 
+    private static final Pattern CAMEL_CASE_PATTERN = Pattern.compile("([a-z]+[A-Z]+\\\\w+)+");
 
     @Override
     public LinterRuleResult check(SchemaDefinition schemaDefinition) {
