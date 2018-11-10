1e5ffaa5e2d1cac65f5f7902b2ffd59aafb78c88
Felipe Reis <felipe.gdr@gmail.com>
2018-11-10 12:04:12 +1100

    Failure: Use PicoCli to parse arguments
    This experiment didn't work. GraalVM seems to ignore, or
    misinterpret some of the annotations used by PicoCli.

--- command-line/src/main/java/graphql/linalyzer/cli/Main.java
+++ command-line/src/main/java/graphql/linalyzer/cli/Main.java
@@ -3,13 +3,33 @@ package graphql.linalyzer.cli;
 import graphql.language.AstPrinter;
 import graphql.language.Document;
 import graphql.parser.Parser;
+import picocli.CommandLine;
 
-public class Main {
+import java.io.File;
 
-    public static void main(String[] args) {
+@CommandLine.Command(name = "lintalyzer", mixinStandardHelpOptions = true, version = "0.0.1")
+public class Main implements Runnable {
+    @CommandLine.Option(names = {"-v", "--verbose"}, description = "Verbose mode. Helpful for troubleshooting.")
+    private boolean verbose;
+
+    @CommandLine.Parameters(arity = "1..*", paramLabel = "FILE", description = "Schema file(s) to process")
+    private File[] inputFiles;
+
+    public void run() {
+        if (verbose) {
+            System.out.println(inputFiles.length + " files to process...");
+        }
+
+        for (File f : inputFiles) {
+            System.out.println(f.getAbsolutePath());
+        }
+
         String schema = "type Query1{foo:String}";
         Document document = new Parser().parseDocument(schema);
         System.out.println(AstPrinter.printAst(document));
     }
 
+    public static void main(String[] args) {
+        CommandLine.run(new Main(), args);
+    }
 }
