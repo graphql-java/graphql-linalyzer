6278bc4c896cd8e3d2f75de712716c5049c95c8c
Andreas Marek <andimarek@fastmail.fm>
2018-11-10 12:55:32 +1100

    fix gitignore

---
+++ library/src/test/groovy/graphql/linalyzer/rules/NamingRuleTest.groovy
@@ -0,0 +1,34 @@
+package graphql.linalyzer.rules
+
+
+import graphql.language.SourceLocation
+import graphql.linalyzer.LinterRuleResult
+import graphql.linalyzer.SchemaDefinition
+import graphql.linalyzer.SchemaDefinitionElement
+import graphql.linalyzer.Severity
+import graphql.parser.Parser
+import spock.lang.Specification
+
+class NamingRuleTest extends Specification {
+
+
+    def "checks for camel case fields"() {
+        given:
+        def sdl = """
+            type Query {
+                Field: String
+            }
+        """
+        def document = new Parser().parseDocument(sdl)
+        SchemaDefinition schemaDefinition = new SchemaDefinition(document)
+        NamingRule namingRuleTest = new NamingRule("ruleId", [SchemaDefinitionElement.FIELD], NamingRule.CAMEL_CASE, Severity.ERROR)
+        when:
+        def result = namingRuleTest.check(schemaDefinition)
+
+        then:
+        result.size() == 1
+        result[0] == new LinterRuleResult(Severity.ERROR, "ruleId", new SourceLocation(3, 17), "Not allowed name Field")
+
+    }
+
+}
